4e1fe76e98b856f1089c77e98f68bc60a035a1e8
Thomas Ardal <thomasardal@gmail.com>
2015-05-16 19:47:54 +0200

    Initial commit

---
+++ elmah.io.umbraco/ElmahIoApplicationEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using Elmah;
+using Umbraco.Core;
+using Umbraco.Web.Routing;
+
+namespace elmah.io.umbraco
+{
+    public class ElmahIoApplicationEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentLastChanceFinderResolver.Current.SetFinder(new NotFoundLastChangeFinder());
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+        }
+    }
+
+    public class NotFoundLastChangeFinder : IContentFinder
+    {
+        public bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            if (contentRequest.Is404)
+            {
+                ErrorSignal.FromCurrentContext().Raise(new HttpException(404, "Page not fount"));
+            }
+
+            return false;
+        }
+    }
+}
