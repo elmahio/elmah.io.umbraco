e599ca4b6690e1021320defe1a512845de31702d
Thomas Ardal <thomasardal@gmail.com>
2022-11-29 16:21:12 +0100

    Ported to Umbraco 10 and up

--- src/elmah.io.umbraco/ElmahIoNotificationMethod.cs
+++ src/elmah.io.umbraco/ElmahIoNotificationMethod.cs
@@ -1,25 +1,34 @@
 ﻿using Elmah.Io.Client;
+using Microsoft.Extensions.Options;
 using System;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using Umbraco.Core.Logging;
-using Umbraco.Web.HealthCheck;
-using Umbraco.Web.HealthCheck.NotificationMethods;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.HealthChecks;
+using Umbraco.Cms.Core.HealthChecks.NotificationMethods;
 
-namespace Elmah.Io.Umbraco
+namespace MyProject
 {
+    /// <summary>
+    /// Health check notification method for Umbraco that log results as elmah.io heartbeats.
+    /// </summary>
     [HealthCheckNotificationMethod("elmah.io")]
     public class ElmahIoNotificationMethod : NotificationMethodBase
     {
         internal static string _assemblyVersion = typeof(ElmahIoNotificationMethod).Assembly.GetName().Version.ToString();
         internal static string _umbracoAssemblyVersion = typeof(NotificationMethodBase).Assembly.GetName().Version.ToString();
-
         private IHeartbeatsClient heartbeats;
-        private readonly ILogger logger;
 
-        public ElmahIoNotificationMethod(ILogger logger)
+        private string apiKey;
+        private string logId;
+        private string heartbeatId;
+
+        /// <summary>
+        /// Create a new instance of the notification method. This constructor should not be called manually but invoked
+        /// automatically by Umbraco when the notification method is correctly configured in appsettings.json.
+        /// </summary>
+        public ElmahIoNotificationMethod(IOptionsMonitor<HealthChecksSettings> healthChecksSettings) : base(healthChecksSettings)
         {
             if (Settings == null)
             {
@@ -27,43 +36,35 @@ namespace Elmah.Io.Umbraco
                 return;
             }
 
-            var apiKey = Settings["apiKey"]?.Value;
-            var logId = Settings["logId"]?.Value;
-            var heartbeatId = Settings["heartbeatId"]?.Value;
+            apiKey = Settings?["apiKey"];
+            logId = Settings?["logId"];
+            heartbeatId = Settings?["heartbeatId"];
             if (string.IsNullOrWhiteSpace(apiKey) || string.IsNullOrWhiteSpace(logId) || string.IsNullOrWhiteSpace(heartbeatId))
             {
                 Enabled = false;
                 return;
             }
-
-            ApiKey = apiKey;
-            LogId = logId;
-            HeartbeatId = heartbeatId;
-
-            this.logger = logger;
         }
 
-        public string ApiKey { get; private set; }
-
-        public string LogId { get; set; }
-
-        public string HeartbeatId { get; set; }
-
-        public override async Task SendAsync(HealthCheckResults results, CancellationToken token)
+        /// <summary>
+        /// Send a health check result to elmah.io as a heartbeat. This method should not be called manually but invoked
+        /// automatically by Umbraco when the notification method is correctly configured in appsettings.json.
+        /// </summary>
+        public override async Task SendAsync(HealthCheckResults results)
         {
             if (ShouldSend(results) == false)
             {
                 return;
             }
 
-            if (string.IsNullOrWhiteSpace(ApiKey) || string.IsNullOrWhiteSpace(LogId) || string.IsNullOrWhiteSpace(HeartbeatId))
+            if (string.IsNullOrWhiteSpace(apiKey) || string.IsNullOrWhiteSpace(logId) || string.IsNullOrWhiteSpace(heartbeatId))
             {
                 return;
             }
 
             if (heartbeats == null)
             {
-                var api = (ElmahioAPI)ElmahioAPI.Create(ApiKey, new ElmahIoOptions
+                var api = (ElmahioAPI)ElmahioAPI.Create(apiKey, new ElmahIoOptions
                 {
                     Timeout = new TimeSpan(0, 0, 30),
                     UserAgent = UserAgent(),
@@ -74,19 +75,11 @@ namespace Elmah.Io.Umbraco
             string result = results.AllChecksSuccessful ? "Healthy" : "Unhealthy";
             var reason = $"Results of the scheduled Umbraco Health Checks run on {DateTime.Now.ToShortDateString()} at {DateTime.Now.ToShortTimeString()} are as follows:\n\n{results.ResultsAsMarkDown(Verbosity)}";
 
-            try
+            await heartbeats.CreateAsync(heartbeatId, logId, new CreateHeartbeat
             {
-                await heartbeats.CreateAsync(HeartbeatId, LogId, new CreateHeartbeat
-                {
-                    Result = result,
-                    Reason = reason,
-                });
-            }
-            catch (Exception e)
-            {
-                logger.Error(GetType(), e);
-                throw;
-            }
+                Result = result,
+                Reason = reason,
+            });
         }
 
         private string UserAgent()
@@ -94,7 +87,7 @@ namespace Elmah.Io.Umbraco
             return new StringBuilder()
                 .Append(new ProductInfoHeaderValue(new ProductHeaderValue("Elmah.Io.Umbraco", _assemblyVersion)).ToString())
                 .Append(" ")
-                .Append(new ProductInfoHeaderValue(new ProductHeaderValue("UmbracoCms.Web", _umbracoAssemblyVersion)).ToString())
+                .Append(new ProductInfoHeaderValue(new ProductHeaderValue("Umbraco.Cms", _umbracoAssemblyVersion)).ToString())
                 .ToString();
         }
     }
