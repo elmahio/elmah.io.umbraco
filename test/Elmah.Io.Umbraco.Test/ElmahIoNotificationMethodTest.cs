0ee57f6927c5708870bfc2d1b388b1c6cf1f7eff
Thomas Ardal <thomasardal@gmail.com>
2023-11-28 09:56:47 +0100

    Ignore test warning

--- test/Elmah.Io.Umbraco.Test/ElmahIoNotificationMethodTest.cs
+++ test/Elmah.Io.Umbraco.Test/ElmahIoNotificationMethodTest.cs
@@ -9,8 +9,10 @@ namespace Elmah.Io.Umbraco.Test
 {
     public class ElmahIoNotificationMethodTest
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         private ElmahIoNotificationMethod sut;
         private IHeartbeatsClient heartbeatsClient;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         [SetUp]
         public void SetUp()
